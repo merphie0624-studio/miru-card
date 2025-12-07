94d6bf25020bff8d7e1176af4cfcb577eb0cd618
merphie0624-studio <merphie0624@gmail.com>
2025-12-07 18:35:54 +0800

    Update script.js

--- free/script.js
+++ free/script.js
@@ -1,25 +1,35 @@
-unction drawFreeCard() {
-    // 13 張 Free 卡的路徑
-    const cards = [
-        "./images/free/free1.png",
-        "./images/free/free2.png",
-        "./images/free/free3.png",
-        "./images/free/free4.png",
-        "./images/free/free5.png",
-        "./images/free/free6.png",
-        "./images/free/free7.png",
-        "./images/free/free8.png",
-        "./images/free/free9.png",
-        "./images/free/free10.png",
-        "./images/free/free11.png",
-        "./images/free/free12.png",
-        "./images/free/free13.png"
-    ];
+// Free 13 張卡的圖片路徑
+const freeCards = [
+"../images/free/free1.png",
+"../images/free/free2.png",
+"../images/free/free3.png",
+"../images/free/free4.png",
+"../images/free/free5.png",
+"../images/free/free6.png",
+"../images/free/free7.png",
+"../images/free/free8.png",
+"../images/free/free9.png",
+"../images/free/free10.png",
+"../images/free/free11.png",
+"../images/free/free12.png",
+"../images/free/free13.png"
+];
 
-    const randomIndex = Math.floor(Math.random() * cards.length);
-    const selectedCard = cards[randomIndex];
+// 點卡片後抽一張 Free 卡
+function drawFreeCard() {
+const coverView = document.getElementById("coverView");
+const cardImage = document.getElementById("cardImage");
+const howTo = document.getElementById("howTo");
 
-    // 直接把區域換成出現抽到的卡
-    document.getElementById("cardArea").innerHTML =
-        `<img src="${selectedCard}" class="card-img">`;
+// 隨機抽一張
+const index = Math.floor(Math.random() * freeCards.length);
+const src = freeCards[index];
+
+// 顯示卡片、隱藏封面
+cardImage.src = src;
+cardImage.classList.remove("hidden");
+coverView.classList.add("hidden");
+
+// 顯示「抽牌小提醒」
+howTo.classList.remove("hidden");
 }
